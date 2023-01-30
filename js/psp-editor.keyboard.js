4353516b7b81f4986ec628c9cfd456369a592abf
jambonbill <jambonbill@gmail.com>
2023-01-30 22:46:02 +0100

    window.keySHIFT=function(){

--- js/psp-editor.keyboard.js
+++ js/psp-editor.keyboard.js
@@ -43,11 +43,29 @@ window.keyCodeToMidiNote=function(keyCode){
 }
 
 
-var _pressedKeys={};
 
 var initKeyboard=function(){
 	//console.info('initKeyboard');	
+
+	let _pressedKeys={};
 	
+	window.keyPressed=function(code){
+		return _pressedKeys[code];
+	}
+
+	
+	window.keySHIFT=function(){
+		return _pressedKeys[16];
+	}
+
+	window.keyCTRL=function(){
+		return _pressedKeys[17];
+	}
+
+	window.keyALT=function(){
+		return _pressedKeys[18];
+	}
+
 	addEventListener("keydown", function(ev) {		
 
 		let c = ev.which;
@@ -76,91 +94,25 @@ var initKeyboard=function(){
 		}
 
 		//Forward keys to the current page
-		switch(AHX.cursor.page){
-			
-			case 0://song
-				songEditor.keydown(ev);
-				break;
-			case 1://phrase
-				phraseEditor.keydown(ev);
-				break;
-			case 2://instrument
-				instrumentEditor.keydown(ev);
-				break;
-		}
-
-		//ev.preventDefault();
-/*
-		if (c == 8) {//backspace
-			//
-		}
-
-		
-		
-		if(c==33)AHX.cursor.pageUp();
-		if(c==34)AHX.cursor.pageDown();
-		if(c==37)AHX.cursor.left();
-		if(c==39)AHX.cursor.right();		
-		if(c==38)AHX.cursor.up();//UP
-		if(c==40)AHX.cursor.down();//DN
-		
-		switch(c){//Other keyboard shortcuts
-
-			case 13://ENTREE
-				AHX.Editor.play();
-				break;
-
-			case 20:// Capslock
-				break;
-		
-			case 27://ESC
-				AHX.Editor.stop()
-				break;
-		
-			case 32:
-				//Toggle Play|Stop
-				AHX.Editor.play();
-				break;
-
-
-			case 35://END
-				break;
-			case 36:// HOME
-				break;
-			
-
-			case 107://PLus +
-				var pos=AHX.Master.Output.Player.PosNr;
-				if(AHX.Song.Positions[AHX.Master.Output.Player.PosNr].Track[AHX.cursor.track]<AHX.Song.TrackNr){
-					AHX.Song.Positions[AHX.Master.Output.Player.PosNr].Track[AHX.cursor.track]++;
-				}
-
-				
-				break;
-			
-			case 109://Minus -
-				var pos=AHX.Master.Output.Player.PosNr;
-				if(AHX.Song.Positions[AHX.Master.Output.Player.PosNr].Track[AHX.cursor.track]>0){
-					AHX.Song.Positions[AHX.Master.Output.Player.PosNr].Track[AHX.cursor.track]--;
-				}
-				break;
-
-			case 111:// - /
-				break;
-			
-			case 188://,
-				break;
-			
-			case 190://.
-				break;
-			
-
-		}
-
-		*/
-
+		switch(AHX.cursor.page){			
+			case 0:songEditor.keydown(ev);	break;
+			case 1:phraseEditor.keydown(ev);break;
+			case 2:instrumentEditor.keydown(ev);break;
+		}		
 
 	});
+
+	addEventListener("keyup",function(ev){
+		let c = ev.which;
+		_pressedKeys[c]=false;
+	});
+
+	window.onblur=function(){
+		console.log("blured!");
+		_pressedKeys={};//release all keys
+	}
 }
+
+
 
 initKeyboard();
