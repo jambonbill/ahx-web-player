80fb41b96f730a56a2e142a6944498eb200493cb
jambonbill <jambonbill@gmail.com>
2023-02-06 23:03:06 +0100

    comments

--- php/protracker.php
+++ php/protracker.php
@@ -15,94 +15,52 @@ use Exception;
  */
 class Protracker{
 
-
+	/**
+	 * source filename
+	 * @var string
+	 */
 	private $filename = ''; 
+	
+	/**
+	 * source filesize
+	 * @var integer
+	 */
 	private $filesize = 0; 
+	
+	/**
+	 * Song name
+	 * @var string
+	 */
 	private $title='';
+	
+	/**
+	 * Protracker Signature. Should be 'M.K.' or 'M!K!','FLT4','FLT8' 
+	 * @var string
+	 */
 	private $MK='';
+	
+	/**
+	 * List of samples
+	 * @var array
+	 */
 	private $samples=[];
-	private $nsp=0;////Number of song positions
-	private $pattern_table=[];
-	private $patterns=[];
 	
 
-	public function periodToNote(int $period)
-	{
-		// Periodtable for Tuning 0, Normal
-	  	// C-0 to B-0 : 1712,1616,1525,1440,1357,1281,1209,1141,1077,1017, 961, 907
-	  	// C-1 to B-1 : 856,808,762,720,678,640,604,570,538,508,480,453
-	  	// C-2 to B-2 : 428,404,381,360,339,320,302,285,269,254,240,226
-	  	// C-3 to B-3 : 214,202,190,180,170,160,151,143,135,127,120,113
-	  	// C-4 to B-4 : 107, 101,  95,  90,  85,  80,  76,  71,  67,  64,  60,  57
-	  	//
-		
-		$table=[];
-		$table[0]=-1;//---
-		$table[1712]=0;//C-0
-		$table[1616]=1;
-		$table[1525]=2;
-		$table[1440]=3;
-		$table[1357]=4;
-		$table[1281]=5;
-		$table[1209]=6;
-		$table[1141]=7;
-		$table[1077]=8;
-		$table[1017]=9;
-		$table[961]=10;
-		$table[907]=11;
-		$table[856]=12;//C-1
-		$table[808]=13;
-		$table[762]=14;
-		$table[720]=15;
-		$table[678]=16;
-		$table[640]=17;
-		$table[604]=18;
-		$table[570]=19;
-		$table[538]=20;
-		$table[508]=21;
-		$table[480]=22;
-		$table[453]=23;
-		$table[428]=24;//C-2
-		$table[404]=25;
-		$table[381]=26;
-		$table[360]=27;
-		$table[339]=28;
-		$table[320]=29;
-		$table[302]=30;
-		$table[285]=31;
-		$table[269]=32;
-		$table[254]=33;
-		$table[240]=34;
-		$table[226]=35;
-		$table[214]=36;//C-3
-		$table[202]=37;
-		$table[190]=38;
-		$table[180]=39;
-		$table[170]=40;
-		$table[160]=41;
-		$table[151]=42;
-		$table[143]=43;
-		$table[135]=44;
-		$table[127]=45;
-		$table[120]=46;
-		$table[113]=47;
-		// C-4 to B-4 : 107, 101,  95,  90,  85,  80,  76,  71,  67,  64,  60,  57
-		return $table[$period];
-	}
-
-
-	public function noteToString(int $note)
-	{
-		if($note<=0){
-			return "---";
-		}
-
-    	$not=$note%12;
-    	$notes=['C-','C#','D-','D#','E-','F-','F#','G-','G#','A-','A#','B-'];
-    	$oct=floor($note/12);
-    	return $notes[$not].$oct;
-	}
-
+	private $nsp=0;//Number of song positions
+	
+	/**
+	 * Song pattern sequence
+	 * @var array
+	 */
+	private $pattern_table=[];
+	
+	/**
+	 * Song pattern data
+	 * @var array
+	 */
+	private $patterns=[];
+	
+	
 
 	public function loadSong(string $filename)
 	{
@@ -118,6 +76,27 @@ class Protracker{
 		fclose($handle);
 	}
 
+
+	/**
+	 * Check Protracker file is valid (contain signature M.K.)
+	 * @param  string $filename [description]
+	 * @return [type]           [description]
+	 */
+	public function is_protracker(string $filename)
+	{
+		//TODO !
+	}
+
+
+	
+	/**
+	 * Return sample data or something like that
+	 * @return [type] [description]
+	 */
+	public function sampleData()
+	{
+		//TODO
+	}
 
 
 	public function decode($handle)
@@ -185,12 +164,12 @@ class Protracker{
 					*/
 				
 					$b1=unpack('C',fread($handle,1))[1];//byte 1
-					$hb1=$b1>>4;//upper 4bits
-					$lb1=$b1& 0x0f;//last 4
+					$hb1=$b1 >> 4;//upper 4bits
+					$lb1=$b1 & 0x0f;//last 4
 					$b2=unpack('C',fread($handle,1))[1];//byte 2 
 					$b3=unpack('C',fread($handle,1))[1];//etc
-					$hb3=$b3>>4;//upper 4bits
-					$lb3=$b3& 0x0f;//last 4
+					$hb3=$b3 >> 4;//upper 4bits
+					$lb3=$b3 & 0x0f;//last 4
 					$b4=unpack('C',fread($handle,1))[1];//...
 					
 					//Decode into Note/Effect/Sample num
@@ -201,14 +180,15 @@ class Protracker{
 					}
 					$dat=['note'=>$note, 'str'=>$this->noteToString($note), 'command'=>$lb3, 'fx'=>$b4, 'samplenum'=>$hb3+$hb1*16];
 
-					$this->patterns[$pat][$row][$track]=$dat;
-					
+					$this->patterns[$pat][$row][$track]=$dat;					
 				}
 			}
 		}
 	}
 
 	
+	
+
 	/**
 	 * Return pattern data
 	 * @param  int    $n [description]
@@ -216,11 +196,16 @@ class Protracker{
 	 */
 	public function pattern(int $n)
 	{
+		if (!$this->patterns[$n]) {
+			throw new Exception("no pattern like #$n", 1);
+		}
 		return $this->patterns[$n];
 	}
 
+	
+
 	/**
-	 * Return a pattern as a pahge of text
+	 * Return a pattern as a page of text
 	 * @param  int    $n [description]
 	 * @return [type]    [description]
 	 */
@@ -270,6 +255,96 @@ class Protracker{
 		//$dat['pattern_table']=$this->pattern_table;
 		$dat['MK']=$this->MK;
 		return $dat;
+	}
+
+
+
+	/**
+	 * Convert a PT period into a note number
+	 * @param  int    $period [description]
+	 * @return [type]         [description]
+	 */
+	private function periodToNote(int $period)
+	{
+		// Periodtable for Tuning 0, Normal
+	  	// C-0 to B-0 : 1712,1616,1525,1440,1357,1281,1209,1141,1077,1017, 961, 907
+	  	// C-1 to B-1 : 856,808,762,720,678,640,604,570,538,508,480,453
+	  	// C-2 to B-2 : 428,404,381,360,339,320,302,285,269,254,240,226
+	  	// C-3 to B-3 : 214,202,190,180,170,160,151,143,135,127,120,113
+	  	// C-4 to B-4 : 107, 101,  95,  90,  85,  80,  76,  71,  67,  64,  60,  57
+	  	//
+		
+		$table=[];
+		$table[0]=-1;//---
+		$table[1712]=0;//C-0
+		$table[1616]=1;
+		$table[1525]=2;
+		$table[1440]=3;
+		$table[1357]=4;
+		$table[1281]=5;
+		$table[1209]=6;
+		$table[1141]=7;
+		$table[1077]=8;
+		$table[1017]=9;
+		$table[961]=10;
+		$table[907]=11;
+		$table[856]=12;//C-1
+		$table[808]=13;
+		$table[762]=14;
+		$table[720]=15;
+		$table[678]=16;
+		$table[640]=17;
+		$table[604]=18;
+		$table[570]=19;
+		$table[538]=20;
+		$table[508]=21;
+		$table[480]=22;
+		$table[453]=23;
+		$table[428]=24;//C-2
+		$table[404]=25;
+		$table[381]=26;
+		$table[360]=27;
+		$table[339]=28;
+		$table[320]=29;
+		$table[302]=30;
+		$table[285]=31;
+		$table[269]=32;
+		$table[254]=33;
+		$table[240]=34;
+		$table[226]=35;
+		$table[214]=36;//C-3
+		$table[202]=37;
+		$table[190]=38;
+		$table[180]=39;
+		$table[170]=40;
+		$table[160]=41;
+		$table[151]=42;
+		$table[143]=43;
+		$table[135]=44;
+		$table[127]=45;
+		$table[120]=46;
+		$table[113]=47;
+		// C-4 to B-4 : 107, 101,  95,  90,  85,  80,  76,  71,  67,  64,  60,  57
+		return $table[$period];
+	}
+
+
+	
+	/**
+	 * Convert a note number to a note String, like 'A#3'
+	 * @param  int    $note [description]
+	 * @return [type]       [description]
+	 */
+	private function noteToString(int $note)
+	{
+		if($note<=0){
+			return "---";
+		}
+
+    	$not=$note%12;
+    	$notes=['C-','C#','D-','D#','E-','F-','F#','G-','G#','A-','A#','B-'];
+    	$oct=floor($note/12);
+    	return $notes[$not].$oct;
 	}
 		
 }
